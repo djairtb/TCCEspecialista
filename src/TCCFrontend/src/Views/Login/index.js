fc3db414658e993db19399cd6903ac651762e92a
djairtb <djairbarral1@gmail.com>
2021-10-13 17:20:30 -0300

    req de login

--- src/TCCFrontend/src/Views/Login/index.js
+++ src/TCCFrontend/src/Views/Login/index.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import Button from "../../Components/Button";
 import Input from "../../Components/Input";
+import Axios from "axios";
 import { FormItem, RegisterLink, FormTitle, Line, Container, Opacity, Form, Subtitle } from "./styles";
+import { Redirect } from "react-router";
 
+function login (event) {
+  Axios({
+    method: "POST",
+    data: {
+      username: this.loginUsername,
+      password: this.loginPassword,
+    },
+    withCredentials: true,
+    url: "http://localhost:3000/login",
+  }).then((res) => {
+    
+  });
 
+};
 function LoginForm() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+ 
   return (
     <Container>
       <Opacity  />
-      <Form>
+      <Form onSubmit={login}>
         <FormTitle>LOGIN</FormTitle>
         <Line />
         <FormItem>
@@ -20,13 +34,14 @@ function LoginForm() {
             width="261px"
             required
             type="text"
-            placeholder="contato@htmlecsspro.com"
+            placeholder="seuLogin"
+            onChange={(e) => setLoginUsername(e.target.value)}
           />
         </FormItem>
         <FormItem>
-          <Input width="261px" required type="password" placeholder="1234" />
+          <Input width="261px" required type="password" placeholder="****" onChange={(e) => setLoginPassword(e.target.value)}/>
         </FormItem>
-        <FormItem>
+        <FormItem> 
           <Button width="261px" type="submit">
             Login
           </Button>
