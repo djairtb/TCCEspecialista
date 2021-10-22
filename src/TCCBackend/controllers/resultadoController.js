7a9436847c408113afcdf6f0cc73bd7a202f30d5
djair.pires <djair.pires@sysevolution.com.br>
2021-10-22 16:49:21 -0300

    wip

--- src/TCCBackend/controllers/resultadoController.js
+++ src/TCCBackend/controllers/resultadoController.js
@@ -1,6 +1,6 @@
 const resultsRepository = require('../repository/resultsRepository');
 
-exports.getUserResults = (req, res, next) => {
+exports.getUserResults = async (req, res, next) => {
     let userid = req.params.user_id;
     let resp = await resultsRepository.getResultByUserID(userid); 
     if(resp)
@@ -9,7 +9,7 @@ exports.getUserResults = (req, res, next) => {
         res.status(505).send('erro ao buscar informações')
 };
 
-exports.getDateRange = (req, res,next) =>{
+exports.getDateRange = async (req, res,next) =>{
     let inicio = req.params.dataInicio;
     let fim = req.params.dataFim;
     let userid = req.params.user_id;
--- src/TCCBackend/routes/resultadoRoute.js
+++ src/TCCBackend/routes/resultadoRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/resultadoController')
 
 router.get('/all', controller.getUserResults);
-router.get('/details', controller.getById);
+router.get('/date', controller.getDateRange);
 
 module.exports = router;
