0ca986e7245f662642815928aa7420690b57a9af
djair.pires <djair.pires@sysevolution.com.br>
2021-10-23 21:16:23 -0300

    wip

--- src/TCCBackend/app.js
+++ src/TCCBackend/app.js
@@ -46,7 +46,8 @@ app.post("/login", (req, res, next) => {
     else {
       req.logIn(user, (err) => {
         if (err) throw err;
-        res.json({ isAuthenticated: true, ...user, token: 'sdfbgvhgfsvhj' });
+        let retorno = {isAuthenticated: true, ...user, token: 'sdfbgvhgfsvhj'}
+        res.json(retorno);
       });
     }
   })(req, res, next);
